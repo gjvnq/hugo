6207ab487a6fa2591378cbe8bc3d2f48fa9483c9
G Queiroz <gabrieljvnq@gmail.com>
2022-11-02 19:45:41 -0300

    chore: add build script for alpine

---
+++ build-alpine.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+docker build --target build -t hugo-alpine .
+CONTAINTER_ID=$(docker create hugo-alpine)
+[ ! -d "build-alpine" ] && mkdir build-alpine
+docker cp ${CONTAINTER_ID}:/go/bin/hugo - > build-alpine/hugo.tar
+cd build-alpine && tar xf hugo.tar && rm hugo.tar
+docker rm -v ${CONTAINTER_ID} > /dev/null
